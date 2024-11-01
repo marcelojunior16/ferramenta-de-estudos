cc32a6abe79c69bd76bdb101c204bf4672e3a2b9
marcelojunior16 <161322824+marcelojunior16@users.noreply.github.com>
2024-11-01 10:35:34 -0300

    Add files via upload

---
+++ ferramenta de estudo/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é html?',
+    ' é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Belgica?',
+    'A capital da Belgica é Bruxelas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'quimica',
+    'o que é ligação saturada?',
+    'são aqueles que possuem somente ligações simples entre os carbonos em sua estrutura.'
+)
